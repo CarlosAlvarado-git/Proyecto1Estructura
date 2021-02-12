5d7acceb45538f83e353c884c68f0b2774071914
Carlos Alvarado <cmalvarado@ufm.edu>
2021-02-12 14:41:02 -0600

    Update api.py

--- api.py
+++ api.py
@@ -4,12 +4,12 @@ File_loader = FileSystemLoader("templates")
 env = Environment(loader=File_loader)
 app = Flask(__name__)
 
-@app.route('/linear/<int:N>/<int:Values>', methods=['GET'])
-def linea_search(N=None,Values=None):
+@app.route('/')
+def index():
     return 
 
-@app.route('/binary/<int:N>/<int:Values>', methods=['GET'])
-def binary_search(N=None,Values=None):
+@app.route('/OPEN')
+def abrir_caja():
    return 
 
 if __name__ == '__main__':
