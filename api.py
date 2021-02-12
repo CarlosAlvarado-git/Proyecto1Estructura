1e0c19e7c0316a69af96c5742cbf5ace5f7c842a
Carlos Alvarado <cmalvarado@ufm.edu>
2021-02-12 17:03:51 -0600

    locuras

--- api.py
+++ api.py
@@ -1,17 +1,30 @@
 from flask import Flask, url_for, request, redirect
 from jinja2 import Template, Environment, FileSystemLoader
+import yaml
 File_loader = FileSystemLoader("templates")
 env = Environment(loader=File_loader)
 app = Flask(__name__)
+with open("banco.yaml") as yaml_file:
+    datos_banco = yaml.load(yaml_file)
 
-@app.route('/')
-def index():
+def concatenar(user, banco):
+
     return 
 
-@app.route('/OPEN')
-def abrir_caja():
-   return 
-@app.route('/DELETE')
+@app.route('/', methods=["POST"])
+def index():
+    if(request.method == "POST"):
+        array = ["","","","","\0"]
+        array[0] = request.form['var1']
+        array[1] = request.form['var2']
+        array[2] = request.form['var3']
+        array[3] = request.form['var4']
+    return 
+@app.route('/cajaabierta/<values>', methods=["GET"])
+def caja():
+
+   return
+@app.route('/DELETE', methods=["GET", "POST"])
 def eliminar_caja():
    return 
 @app.route('/LIST')
