35b15ea40127a77d4eaa26c10b1c454bde1c8503
mp4402 <mariopisquiy@ufm.edu>
2021-02-12 20:04:24 -0600

    Update api.py

--- api.py
+++ api.py
@@ -8,18 +8,43 @@ with open("banco.csv") as File:
     datos_banco = csv.reader(File)
 
 def concatenar(user, banco):
+    clave = []
+    contador = 0
+    for i in user:
+        if user[i] != "\0":
+            clave[contador] = user[i]
+            contador += 1
+    clave[contador] = "-" 
+    contador += 1       
+    for i in banco:
+        if banco[i] != "\0":
+            clave[contador] = banco[i]
+            contador += 1
+    clave[contador] = "\0"
+    return clave
 
-    return 
+def formarString(clave):
+    string = ""
+    clave = clave[:-1]
+    for i in clave:
+        clave += i
+    return clave
 
-@app.route('/', methods=["POST"])
+@app.route('/', methods=["GET","POST"])
 def index():
     if(request.method == "POST"):
         array = ["","","","","\0"]
+        banco = ["B","C","5","2","\0"]
         array[0] = request.form['var1']
         array[1] = request.form['var2']
         array[2] = request.form['var3']
         array[3] = request.form['var4']
-    return 
+        clave = concatenar(array, banco)
+        claveRedireccion = formarString(clave)
+        return redirect(f"http://localhost:5000/caja_abierta/{claveRedireccion}")
+    template = env.get_template('index.html')
+    return template.render()
+
 @app.route('/caja_abierta/<values>', methods=["GET"])
 def abierta():
 
@@ -35,7 +60,7 @@ def eliminar_caja():
 def listas_cajas():
    return 
 @app.route('/Crear_Caja')
-def Crear_caja():
+def Crear_caja(nombre, clave):
     with open('banco.csv', mode='w') as b_file:
         banco_writer = csv.writer(b_file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
