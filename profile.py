842bd29bc6ff6c878c1ad731c3af100f66e06988
Carlos Alvarado <cmalvarado@ufm.edu>
2021-02-26 17:24:56 -0600

    cambio ruta

---
+++ profile.py
@@ -0,0 +1,8 @@
+#!flask/bin/python
+#import cProfile
+#from werkzeug.contrib.profiler import ProfilerMiddleware
+#from api import C_caja
+
+#C_caja.config['PROFILE'] = True
+#C_caja.wsgi_app = ProfilerMiddleware(C_caja.wsgi_app, restrictions=[30])
+#C_caja.run(debug = True)
