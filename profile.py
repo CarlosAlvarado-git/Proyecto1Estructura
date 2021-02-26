faa6af2a94c43d1379d96a4e36ce9d91e8511c38
Carlos Alvarado <cmalvarado@ufm.edu>
2021-02-26 17:55:03 -0600

    CAMBIOS DE PROFILE

--- profile.py
+++
@@ -1,8 +0,0 @@
-#!flask/bin/python
-#import cProfile
-#from werkzeug.contrib.profiler import ProfilerMiddleware
-#from api import C_caja
-
-#C_caja.config['PROFILE'] = True
-#C_caja.wsgi_app = ProfilerMiddleware(C_caja.wsgi_app, restrictions=[30])
-#C_caja.run(debug = True)
